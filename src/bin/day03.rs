8217c3108f8ed19e24fbbb67ad9d4f94333e6fe2
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2023-12-03 16:34:08 +1000

    Solve day03 part1 and part2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,9 +1,30 @@
+use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
+
+use fancy_regex::Regex;
+use lazy_static::lazy_static;
+
+use aoc_utils::cartography::Point2D;
 
 const PROBLEM_NAME: &str = "Gear Ratios";
 const PROBLEM_INPUT_FILE: &str = "./input/day03.txt";
 const PROBLEM_DAY: u64 = 3;
+
+lazy_static! {
+    /// Matches any string containing one or more digits in sequence
+    static ref REGEX_NUMBER: Regex = Regex::new(r"(\d+)").unwrap();
+    /// Matches any single character that is not '.' or a digit
+    static ref REGEX_SYMBOL: Regex = Regex::new(r"([^\.\d])").unwrap();
+}
+
+#[derive(Copy, Clone)]
+struct Number {
+    value: u64,
+    start: usize,
+    end: usize,
+    counted: bool,
+}
 
 /// Processes the AOC 2023 Day 03 input file and solves both parts of the problem. Solutions are
 /// printed to stdout.
@@ -39,27 +60,123 @@ pub fn main() {
 }
 
 /// Processes the AOC 2023 Day 03 input file in the format required by the solver functions.
-/// 
-/// Returned value is ###.
-fn process_input_file(filename: &str) -> String {
+///
+/// Returned value is HashMap mapping locations to the component held at the location in the engine
+/// schematic.
+fn process_input_file(filename: &str) -> (Vec<Vec<Number>>, HashMap<Point2D, char>) {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    let mut number_data: Vec<Vec<Number>> = vec![];
+    let mut symbol_locs: HashMap<Point2D, char> = HashMap::new();
+    for (y, row) in raw_input
+        .lines()
+        .map(|line| line.trim())
+        .filter(|line| !line.is_empty())
+        .enumerate()
+    {
+        let mut current_row: Vec<Number> = vec![];
+        // Find numbers in current row
+        for number_match in REGEX_NUMBER.find_iter(row) {
+            let number_match = number_match.unwrap();
+            let value = number_match.as_str().parse::<u64>().unwrap();
+            let number_comp = Number {
+                value,
+                start: number_match.start(),
+                end: number_match.end() - 1,
+                counted: false,
+            };
+            current_row.push(number_comp);
+        }
+        number_data.push(current_row);
+        // Find symbols in current row
+        for symbol_match in REGEX_SYMBOL.find_iter(row) {
+            let symbol_match = symbol_match.unwrap();
+            let loc = Point2D::new(
+                i64::try_from(symbol_match.start()).unwrap(),
+                i64::try_from(y).unwrap(),
+            );
+            let c = symbol_match.as_str().chars().next().unwrap();
+            symbol_locs.insert(loc, c);
+        }
+    }
+    (number_data, symbol_locs)
 }
 
 /// Solves AOC 2023 Day 03 Part 1.
-/// 
-/// ###
-fn solve_part1(_input: &String) -> u64 {
-    unimplemented!();
+///
+/// Add up the total of all part numbers from the engine schematic.
+fn solve_part1(input: &(Vec<Vec<Number>>, HashMap<Point2D, char>)) -> u64 {
+    let mut numbers = input.0.clone();
+    let symbol_locs = &input.1;
+    // Calculate part number sum
+    let mut part_sum = 0;
+    for loc in symbol_locs.keys() {
+        for s_loc in loc.get_surrounding_points() {
+            // Look for number in row of the surrounding point
+            let check_row = usize::try_from(s_loc.y()).unwrap();
+            let check_col = usize::try_from(s_loc.x()).unwrap();
+            for number in numbers[check_row].iter_mut() {
+                // Check if the number has already been counted
+                if number.counted {
+                    continue;
+                }
+                // Check if the number is adjacent to the symbol
+                if number.start <= check_col && number.end >= check_col {
+                    part_sum += number.value;
+                    number.counted = true;
+                }
+            }
+        }
+    }
+    part_sum
 }
 
 /// Solves AOC 2023 Day 03 Part 2.
-/// 
-/// ###
-fn solve_part2(_input: &String) -> u64 {
-    unimplemented!();
+///
+/// Finds the sum of all gear ratios in the engine schematic. Gear ratios are found by calculating
+/// the product of the two values adjacent to a '*' symbol, where only two values are adjacent to
+/// the symbol.
+fn solve_part2(input: &(Vec<Vec<Number>>, HashMap<Point2D, char>)) -> u64 {
+    let symbol_locs = &input.1;
+    // Calculate gear ratio sum
+    let mut gear_ratio_sum = 0;
+    'outer: for (&loc, &symbol) in symbol_locs {
+        // Skip any symbols that are not '*'
+        if symbol != '*' {
+            continue;
+        }
+        // Refresh the state of numbers for each '*' symbol
+        let mut numbers = input.0.clone();
+        let mut part_numbers: Vec<u64> = vec![];
+        for s_loc in loc.get_surrounding_points() {
+            // Look for number in row of the surrounding point
+            let check_row = usize::try_from(s_loc.y()).unwrap();
+            let check_col = usize::try_from(s_loc.x()).unwrap();
+            for number in numbers[check_row].iter_mut() {
+                // Do not double-count numbers for the current '*' - independent of other '*'
+                if number.counted {
+                    continue;
+                }
+                // Check if part number is next to the '*' symbol
+                if number.start <= check_col && number.end >= check_col {
+                    number.counted = true;
+                    part_numbers.push(number.value);
+                }
+                // Check if too many part numbers are next to the '*' symbol
+                if part_numbers.len() > 2 {
+                    continue 'outer;
+                }
+            }
+        }
+        // Check if correct number of gear values is next to the symbol
+        if part_numbers.len() != 2 {
+            continue 'outer;
+        }
+        // Add the gear ratio for the current gear
+        gear_ratio_sum += part_numbers.iter().product::<u64>();
+    }
+    gear_ratio_sum
 }
 
 #[cfg(test)]
@@ -70,17 +187,31 @@ mod test {
     #[test]
     fn test_day03_part1_actual() {
         let input = process_input_file(PROBLEM_INPUT_FILE);
-        let _solution = solve_part1(&input);
-        unimplemented!();
-        // assert_eq!("###", solution);
+        let solution = solve_part1(&input);
+        assert_eq!(544664, solution);
     }
 
     /// Tests the Day 03 Part 2 solver method against the actual problem solution.
     #[test]
     fn test_day03_part2_actual() {
         let input = process_input_file(PROBLEM_INPUT_FILE);
-        let _solution = solve_part2(&input);
-        unimplemented!();
-        // assert_eq!("###", solution);
+        let solution = solve_part2(&input);
+        assert_eq!(84495585, solution);
+    }
+
+    /// Tests the Day 03 Part 1 solver method against the 01 test input.
+    #[test]
+    fn test_day03_part1_ex01() {
+        let input = process_input_file("./input/test/day03_01.txt");
+        let solution = solve_part1(&input);
+        assert_eq!(4361, solution);
+    }
+
+    /// Tests the Day 03 Part 2 solver method against the 01 test input.
+    #[test]
+    fn test_day03_part2_ex01() {
+        let input = process_input_file("./input/test/day03_01.txt");
+        let solution = solve_part2(&input);
+        assert_eq!(467835, solution);
     }
 }
